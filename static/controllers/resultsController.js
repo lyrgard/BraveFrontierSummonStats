bb8356771e806fb5f1f342590547c2abff1d0fba
Lyrgard <lyrgard@gmail.com>
2016-06-02 14:37:48 +0200

    added images

--- static/controllers/resultsController.js
+++ static/controllers/resultsController.js
@@ -6,15 +6,46 @@ angular.module('bfSS').controller('bfSSResultsController', [
         var getFullLabel = function(label, value, total) {
             var percentage = Math.floor(((value/total) * 100)+0.5);
             var interval = Math.floor(1960*Math.sqrt((value/total*(1-value/total))/total))/10;
-            return label + " : " + percentage + "% (+/- " + interval + "%)";
+            return label + " : " + percentage + "% (+/- " + interval + '%)';
         };
+        
+        var getNewChartConfig = function(label) {
+            return {
+                options: {
+                    chart: {type: 'pie'},
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                useHTML: true,
+                                distance: 40,
+                                y: -10
+                            }
+                        }
+                    }
+                },
+                /*plotOptions: {
+                    series : {
+                        dataLabels: {
+                            enabled: true,
+                            format: 'to {y} <img src="http://vignette3.wikia.nocookie.net/bravefrontierrpg/images/6/65/Unit_ills_thum_50166.png/revision/latest/scale-to-width-down/42?cb=20160106141141&path-prefix=fr"></img>',
+                            useHTML: true
+                        }
+                    }
+                },*/
+                series: [
+                    {data: []}
+                ],
+                title: {text:label},
+                loading: false
+            };
+        }
         
         var getData = function(id) {
             $http.get('data/' + id + '-schema.json').success(function(schema) {
                 $http.get('data/' + id + '-data.json').success(function(data) {
                     $scope.title = schema.name;
                     $scope.chartConfigs = [];
-                    $scope.chartConfigs[0] = {options: {chart: {type: 'pie'}},series: [{data: []}],title: {text:""},loading: false};
+                    $scope.chartConfigs[0] = getNewChartConfig("");
                     var total = 0;
                     for (i = 0; i < schema.groupLabels.length; i++) {
                         var label = schema.groupLabels[i];
@@ -23,7 +54,7 @@ angular.module('bfSS').controller('bfSSResultsController', [
                         if (groupDef == null) {
                             value = data['unit-' + i + '-0'];
                         } else {
-                            var subGroupChartConfig = {options: {chart: {type: 'pie'}},series: [{data: []}],title: {text:label},loading: false};
+                            var subGroupChartConfig = getNewChartConfig(label);
                             // first calculate total :
                             var subGroupTotal = 0;
                             for (j = 0; j < groupDef.labels.length; j++) {
