522b7a92b78593dd2616258f2e2f5c7515b621ec
Lyrgard <lyrgard@gmail.com>
2016-06-03 16:02:13 +0200

    added RU Zodiac 3

--- static/controllers/addDataController.js
+++ static/controllers/addDataController.js
@@ -11,6 +11,21 @@ angular.module('bfSS').controller('bfSSAddDataController', [
         $scope.model = {};
         
         $scope.submit = function() {
-            $scope.result = JSON.stringify($scope.model);   
+            var data = encodeURIComponent(JSON.stringify($scope.model));
+            var text = '';
+            for (i = 0; i < $scope.schema.groups.length; i++) {
+                var group = $scope.schema.groups[i];
+                if (group == null) {
+                    text += '[b]' + ($scope.model['unit-' + i + '-0'] || 0) + ' ' + $scope.schema.groupLabels[i] + '[/b]\n';
+                } else {
+                    text += '[b]' + $scope.schema.groupLabels[i] + '[/b]\n[LIST]\n';
+                    for (j = 0; j < group.labels.length; j++) {
+                        text += '[*]' + ($scope.model['unit-' + i + '-' + j] || 0) + ' ' + group.labels[j] + '\n';
+                    }
+                    text += '[/LIST]\n';
+                }
+            }
+            text = text.replace(/<img.*<\/img>/g, '');
+            window.prompt("Copy this text to the forum", text);
         }
 }]);
